20e092b935352d727c522787c51d1e5c0850a437
lucif680 <pratikmazumdar680@protonmail.com>
2020-04-08 23:29:29 +0530

    Update

--- js/main.js
+++ js/main.js
@@ -80,7 +80,6 @@ $(document).ready(function(){
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('#mobile-nav-toggle i').toggleClass('lnr-cross lnr-menu');
           $('#mobile-body-overly').fadeOut();
         }
       }
